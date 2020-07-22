1012f059183325521fb23e08bfe6aac4a96e6fe1
krosa100 <krosa100@gmail.com>
2020-07-22 04:45:20 -0500

    n

--- script.js
+++ script.js
@@ -22,6 +22,29 @@ function text(words, color, size_v, left1_v, left2_v, top_v) {
     document.body.appendChild(e)
 }
 
+function link(words,color, size_v, left1_v, left2_v, top_v, link) {
+    var left1 = x(left1_v)
+    var left2 = x(left2_v)
+    var top = y(top_v)
+    var size = s(size_v)
+
+    var e = document.createElement('a')
+    e.href = link
+    //e.style.backgroundColor = 'gray'
+    e.style.position = 'absolute'
+    e.style.width = px(left2 - left1)
+
+    e.style.left = px(left1)
+    e.style.top = px(top)
+    e.style.fontFamily = 'Open Sans'
+    e.style.fontSize = px(size)
+    e.style.color = color
+    e.innerHTML = words
+    e.style.lineHeight = px(size)
+
+    document.body.appendChild(e)
+}
+
 function box(color, left1_v, left2_v, top1_v, top2_v, fixed) {
     var top1 = y(top1_v)
     var top2 = y(top2_v)
@@ -49,7 +72,6 @@ function box(color, left1_v, left2_v, top1_v, top2_v, fixed) {
     document.body.appendChild(e)
 }
 
-
 function img(src, left1_v, left2_v, top1_v, fixed) {
     var left1 = x(left1_v)
     var left2 = x(left2_v)
@@ -72,11 +94,20 @@ function img(src, left1_v, left2_v, top1_v, fixed) {
 }
 
 function btn() {
-    var form = document.getElementsByTagName('form')[0];
-    form.style.zIndex = 2
+    var form = document.createElement('form')
+    form.action = 'https://formspree.io/xlepagvr'
+    form.method = 'POST'
 
-    var email = form.children[0]
-    var submit = form.children[1]
+    var email = document.createElement('input')
+    email.type = 'email'
+    email.name = "_replyto"
+    email.placeholder = 'email'
+
+    var submit = document.createElement('input')
+    submit.type = 'submit'
+    submit.value = 'sign up for the beta!'
+
+    form.style.zIndex = 2
 
     submit.style.backgroundColor = gray4
     submit.style.border = 'none'
@@ -86,6 +117,7 @@ function btn() {
     submit.style.color = 'white'
     submit.style.fontSize = px(s(20))
     submit.style.fontFamily = 'Open Sans'
+    submit.style.cursor = 'pointer'
     
     submit.style.position = 'absolute'
     submit.style.top = px(y(0.61 * h))
@@ -104,7 +136,9 @@ function btn() {
     email.style.left = px(x(w / 4))
     email.style.zIndex = 2;
 
-
+    form.appendChild(email)
+    form.appendChild(submit)
+    document.body.appendChild(form)
 }
 
 function px(n) {
@@ -125,26 +159,8 @@ function y(py) {
     return py * fac
 }
 
-var tw = window.innerWidth
-var th = window.innerHeight
-
-var minwidth = 1100
-var fac = 1
-if (tw < minwidth) {
-    fac = tw / minwidth
-
-    //document.body.style.transform = 'scale(' + tw/minwidth + ')';
-    //document.body.style.transformOrigin = tw / 2 + " 0";
-}
-
-var mx = 1 / 4 * w;
-var my = 1 / 4 * h;
-
 var w = 1920
 var h = 1080
-
-console.log(tw)
-console.log(x(w))
 
 var gray1 = '#E6E6E6'
 var gray2 = '#e1e1e1'
@@ -159,34 +175,65 @@ var black = 'black'
 var big = 50
 var norm = 30
 
-btn()
-//background
-document.body.style.backgroundColor = gray1
-document.body.style.margin = px(0)
-
 var t1 = "Objective's browser extension lets you choose what you see online."
 var t2 = 'You tell Objective the sort of content you want to see.'
-var t3 = 'Objective uses ML to label items, then filters and resorts them accordingly.'
+var t3 = 'Objective uses ML to label search results and social media items, then filters and resorts them accordingly.'
+var t4 = 'email us!'
 
-box(gray1, 0, w, 0, h, false)
-box(gray2, 0, w, h, 1.75 * h, false)
-box(gray3, 0, w, 1.75 * h, 2.5 * h, false)
-box(darkgray, 0, w, 2.5 * h, 3.5 * h, false)
+var tw = null
+var th = null
+var fac = null
 
-text(t1, black, big, 1/4 * w, 1/2 * w, 1/3 * h)
-img('p1.svg', 1 / 2 * w, 3 / 4 * w, 1 / 4 * h)
+function start() {
+    document.body.innerHTML = ''
 
-img('tablet.svg', 0.25 * w, 0.42 * w, 1.175 * h)
-text(t2, black, norm, 0.45 * w, 0.75 * w, 1.325 * h)
+    tw = window.innerWidth
+    th = window.innerHeight
 
-img('up.svg', 0.3 * w, 0.7 * w, 2.025 * h)
-text(t3, black, norm, 0.25 * w, 0.75 * w, 1.925 * h)
+    var minwidth = 1100
+    fac = 1
+    if (tw < minwidth) {
+        fac = tw / minwidth
 
-img('goodbye.svg', 0.45 * w, 0.55 * w, 3.0 * h)
+        //document.body.style.transform = 'scale(' + tw/minwidth + ')';
+        //document.body.style.transformOrigin = tw / 2 + " 0";
+    }
 
-box(gray1, 0,w ,0, 0.067 * h, true)
-box(gray3, 0,w,0.067 * h, 0.068 * h, true)
-img('logo.svg', 1 / 4 * w, 0.345 * w, 5, true)
+    //background
+    document.body.style.backgroundColor = gray1
+    document.body.style.margin = px(0)
 
-document.body.style.height = '100%'
 
+    box(gray1, 0, w, 0, h, false)
+    box(gray2, 0, w, h, 1.75 * h, false)
+    box(gray3, 0, w, 1.75 * h, 2.5 * h, false)
+    box(darkgray, 0, w, 2.5 * h, 3.5 * h, false)
+
+    text(t1, black, big, 1 / 4 * w, 1 / 2 * w, 1 / 3 * h)
+    img('p1.svg', 1 / 2 * w, 3 / 4 * w, 1 / 4 * h)
+
+    img('tablet.svg', 0.25 * w, 0.42 * w, 1.175 * h)
+    text(t2, black, norm, 0.45 * w, 0.75 * w, 1.325 * h)
+
+    img('up.svg', 0.3 * w, 0.7 * w, 2.025 * h)
+    text(t3, black, norm, 0.25 * w, 0.75 * w, 1.925 * h)
+
+    img('goodbye.svg', 0.40 * w, 0.60 * w, 3.0 * h)
+
+    box(gray1, 0, w, 0, 0.067 * h, true)
+    box(gray3, 0, w, 0.067 * h, 0.068 * h, true)
+    img('logo.svg', 1 / 4 * w, 0.345 * w, 5, true)
+    text('our email is', black, norm, 0.45 * w, 0.75 * w, 3.01 * h)
+    link('getobjective@gmail.com', blue, norm, 0.41 * w, 0.45 * w, 3.04 * h, 'mailto:getobjective@gmail.com?subject=Tell me more!!!')
+
+    document.body.style.height = '100%'
+
+    btn()
+}
+start()
+
+window.addEventListener('resize', function (event) {
+    console.log('pls stop')
+
+    start()
+});
